8329318f9e953ccc9c0cb93eb27ffe8dc18b928b
Tony Nguyen <tntuyen@ucdavis.edu>
2017-01-17 16:59:20 -0800

    add bootstrap, redux and react-redux dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
 
 ReactDOM.render(
-  <App />,
+	<Provider store={createStore(reducers)}>
+  	<App />
+  </Provider>,
   document.getElementById('root')
 );
