d5a4fab15268d0f8953e04711e46c85798773eed
Tony Nguyen <tntuyen@ucdavis.edu>
2017-03-01 16:40:22 -0800

    add sparkline chart and trendline for averages

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/Chart';
 
 class WeatherList extends Component {
 	renderWeather(cityData) {
-		const name = cityData.city.name
+		const name = cityData.city.name;
+		const temps = cityData.list.map((weather) => weather.main.temp);
+		const humidities = cityData.list.map((weather) => weather.main.humidity);
+		const pressures = cityData.list.map((weather) => weather.main.pressure);
+
 		return (
 			<tr key={name}>
 				<td>{name}</td>
+				<td><Chart color="green" data={temps} /></td>
+				<td><Chart color="black" data={humidities} /></td>
+				<td><Chart color="blue" data={pressures} /></td>
 			</tr>
-		)
+		);
 	}
 
 	render() {
+		console.log(this.props.weather)
 		return (
 			<table className="table table-hover">
 				<thead>
